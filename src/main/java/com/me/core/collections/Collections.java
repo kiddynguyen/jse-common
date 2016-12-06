67f1d33f062b51b24570a06bb13e89481e7b905d
kiddy <rankynguyen@gmail.com>
2016-12-06 11:03:44 +0700

    added lock

--- src/main/java/com/me/core/collections/Collections.java
+++ src/main/java/com/me/core/collections/Collections.java
@@ -1,11 +1,15 @@
 package com.me.core.collections;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
+import java.util.TreeMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -18,6 +22,10 @@ public class Collections {
     static List<Integer> arrayList = new ArrayList<>();
     // no limit size
     static List<Integer> linkedList = new LinkedList<>();
+    
+    static Map<String, Object> map = new Hashtable<>();
+    static Map<String, Object> map2 = new ConcurrentHashMap<>();
+    static Map<String, Object> tmap = new TreeMap<>();
     
     // size of blocking queue is fixed, not able resizable
     // linkedqueue: if not pass initial size then default capacity = Integer.MAX_VALUE
