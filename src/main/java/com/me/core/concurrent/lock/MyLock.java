6b70bdea6477b8e3489f3fdc06c35ab817003f1c
kiddy <rankynguyen@gmail.com>
2016-12-08 14:09:59 +0700

    update

---
+++ src/main/java/com/me/core/concurrent/lock/MyLock.java
@@ -0,0 +1,21 @@
+package com.me.core.concurrent.lock;
+
+/**
+ *
+ * @author tunm2
+ */
+public class MyLock {
+
+    private boolean locked = false;
+
+    public synchronized void lock() throws InterruptedException {
+        while (locked)
+            wait();
+        locked = true;
+    }
+
+    public synchronized void unlock() {
+        locked = false;
+        notify();
+    }
+}
---
+++ src/main/java/com/me/core/concurrent/signal/ThreadSignaling.java
@@ -0,0 +1,77 @@
+package com.me.core.concurrent.signal;
+
+/**
+ *
+ * @author tunm2
+ */
+public class ThreadSignaling {
+
+    /**
+     * A thread signaling mechanic using volatile variable and infinite loop. 
+     * This approach is not good for CPU utilization due to the waiting thread is active
+     */
+    class BusyWaitSignaling {
+        private volatile boolean signalled = false;
+
+        public void doWait() {
+            while (!signalled) {
+            }
+        }
+
+        public void doNotify() {
+            signalled = true;
+        }
+    }
+    
+    /**
+     * Two problems:
+     *  1 Missed signal (http://tutorials.jenkov.com/java-concurrency/thread-signaling.html)
+     *  2 Spurious wakeup (http://tutorials.jenkov.com/java-concurrency/thread-signaling.html)
+     */
+    class WaitNotifySignaling1 {
+        public synchronized void doWait() throws InterruptedException {
+            wait();
+        }
+        
+        public synchronized void doNotify() {
+            notify();
+        }
+    }
+    
+    /**
+     * One problem:
+     *   Spurious wakeup (http://tutorials.jenkov.com/java-concurrency/thread-signaling.html)
+     */
+    class WaitNotifySignaling2 {
+        private boolean signalled = false;
+
+        public synchronized void doWait() throws InterruptedException {
+            if (!signalled) {
+                wait();
+            }
+        }
+
+        public synchronized void doNotify() {
+            signalled = true;
+            notify();
+        }
+    }
+    
+    /**
+     * Overcome two issues above
+     */
+    class WaitNotifySignaling3 {
+        private boolean signalled = false;
+
+        public synchronized void doWait() throws InterruptedException {
+            while (!signalled) {
+                wait();
+            }
+        }
+
+        public synchronized void doNotify() {
+            signalled = true;
+            notify();
+        }
+    }
+}
