ad14fd318c9549c5904c36422aab410261f376cb
kiddy <rankynguyen@gmail.com>
2017-06-15 18:07:02 +0700

    implemented DiskBasedList

---
+++ src/main/java/com/me/advance/storage/Serializer.java
@@ -0,0 +1,12 @@
+package com.me.advance.storage;
+
+/**
+ *
+ * @author tunm2
+ */
+public interface Serializer {
+    
+    public byte[] serialize(Object value);
+    
+    public <T> T deserialize(byte[] bytes);
+}
